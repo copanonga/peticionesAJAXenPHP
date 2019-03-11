9f9ab83f293a9f7433dc7aa42cae011cf8a24cff
Copanonga <35576851+copanonga@users.noreply.github.com>
2019-03-11 09:21:43 +0100

    Obtenidas variables y creado el JSON a devolver

--- demo.php
+++ demo.php
@@ -1,1 +1,19 @@
+<?php
 
+    if (isset($_POST['dato1']) && $_POST['dato2']) {
+
+        $response = array();
+        $response["dato1"] = $_POST['dato1'];
+        $response["dato2"] = $_POST['dato2'];
+        $response["success"] = 1;
+        
+        echo json_encode($response);
+        
+    } else {
+        
+        $response = array();
+        $response["success"] = 0;
+        
+        echo json_encode($response);
+        
+    }
